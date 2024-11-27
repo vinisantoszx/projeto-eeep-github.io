df53b7961d2a5cfd2c7ce7ebb0c1976138ab1a4c
Felipe ツ <antonio.pereira312@aluno.ce.gov.br>
2024-11-27 10:09:26 -0300

    Update mobile-navbar.js

--- JS/mobile-navbar.js
+++ JS/mobile-navbar.js
@@ -65,11 +65,12 @@ document.addEventListener("DOMContentLoaded", () => {
         window.location.href = "login.html"; // Redireciona para a página de login
     });
 });
-document.addEventListener("DOMContentLoaded", () => {
-    const isLoggedIn = localStorage.getItem("isLoggedIn");
 
-    if (isLoggedIn !== "true") {
-        alert("Você precisa fazer login para acessar esta página.");
-        window.location.href = "login.html"; // Redireciona para a página de login
-    }
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//    const isLoggedIn = localStorage.getItem("isLoggedIn");
+
+//    if (isLoggedIn !== "true") {
+//        alert("Você precisa fazer login para acessar esta página.");
+//        window.location.href = "login.html"; // Redireciona para a página de login
+//    }
+//});
